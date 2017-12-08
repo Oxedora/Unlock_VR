0c8dffcf1d769987ce3d95316a138b1de0d83782
celia.rouquairol@etu.umontpellier.fr <celia.rouquairol@etu.umontpellier.fr>
2017-12-08 17:54:31 +0100

    prefab panneau elec, script coffre, couleur diode

---
+++ Assets/_Scripts/UnlockChest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class UnlockChest : MonoBehaviour {
+	// Tag of the object that unlock the chest
+	private String objectTagToUnlock = "Key";
+
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log("TRIGGERED");
+		GameObject collidedObject = other.gameObject;
+		if(collidedObject.tag.Equals(objectTagToUnlock)){
+			Destroy(collidedObject);
+			// lancer l'ouverture du coffre
+			// éventuellement une petite musique fort sympathique
+		}
+	}
+}
